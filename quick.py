0f06d86c32736520b1ca021aa811ea50c6683d90
Tyler Long <tyler4long@gmail.com>
2012-01-19 14:03:12 +0800

    change default project name to 'myproject'

--- quick.py
+++ quick.py
@@ -10,7 +10,7 @@ from toolkit_library.input_util import InputUtil
 
 def create_project():
     """Create a new project"""
-    project_name = InputUtil.get_input('project name', default = 'test', pattern = '[a-z][a-z_]{,63}')
+    project_name = InputUtil.get_input('project name', default = 'myproject', pattern = '[a-z][a-z_]{,63}')
     if os.path.exists(project_name):
         if os.path.isdir(project_name):
             print 'a folder with the same name already exists in the current directory'
